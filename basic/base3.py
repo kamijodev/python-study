0aba024ebad9e3ffa37da4339b8c9c053929c721
kamijodev <kamijodev@gmail.com>
2020-11-28 23:49:25 +0900

    add: base3.py

---
+++ basic/base3.py
@@ -0,0 +1,15 @@
+# 論理型
+is_animal = True
+if is_animal:
+    print('動物です')
+
+is_man = True
+is_adult = False
+
+# or
+if is_man or is_adult:
+    print('男か大人です')
+
+# and
+if is_man and is_adult:
+    print('成人男性です')
