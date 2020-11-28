c5f6863fb077add2ae47c737cb9f44b088f2a9a1
kamijodev <kamijodev@gmail.com>
2020-11-28 22:20:48 +0900

    init

---
+++ basic/base1.py
@@ -0,0 +1,17 @@
+# 標準入出力、コメント、変数
+
+print('Hello World')
+
+# コメント
+
+"""
+あああ
+いいい
+"""
+
+name = input('あなたの名前は何ですか?: ')
+print(name)
+age = 18
+
+# format関数
+print('私の名前は{} 年齢は{}'.format(name, age))
