8cefd15366d36a4cb49594e8483def1f546133fc
kamijodev <kamijodev@gmail.com>
2020-11-29 14:00:37 +0900

    continue base7

--- basic/base7.py
+++ basic/base7.py
@@ -28,3 +28,42 @@ print(type(byte_fruit))
 
 str_fruit = byte_fruit.decode('utf-8')
 print(type(str_fruit))
+
+# count
+
+msg = 'ABCDRABC'
+print(msg.count('ABC'))
+
+# startswith, endswith
+
+print(msg.startswith('ABCD'))
+print(msg.endswith('DC'))
+
+# strip, rstrip, lstrip
+
+msg = ' ABC '
+print(msg)
+print(msg.strip())
+msg = 'AABAABAA'
+print(msg.strip('A'))
+msg = 'AABCAACBAA'
+print(msg.strip('BA'))
+print(msg.lstrip('BA'))
+print(msg.rstrip('BA'))
+
+# upper, lower, swapcase, replace, capitalize
+
+msg = 'abcABC'
+msg_u = msg.upper()
+msg_l = msg.lower()
+msg_s = msg.swapcase()
+print(msg_u, msg_l, msg_s)
+
+msg = 'ABCDEABC'
+msg_r = msg.replace('ABC', 'FFF')
+print(msg_r)
+msg_r = msg.replace('ABC', 'FFF', 1)
+print(msg_r)
+
+msg = 'helLo wOrld'
+print(msg.capitalize())
